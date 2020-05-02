21d94bcc517ba1153e65dcc41112d91acba94663
Olivier GAUDEFROY <ogaudefroy@vente-privee.com>
2020-05-02 15:50:45 +0200

    Traits

--- src/koans/traits.rs
+++ src/koans/traits.rs
@@ -15,7 +15,7 @@ fn implementing_traits() {
         fn full_name(&self) -> String;
     }
 
-    impl Person {
+    impl HasName for Person {
         fn full_name(&self) -> String {
             format!("{} {}", self.first_name, self.last_name)
         }
@@ -57,6 +57,10 @@ fn implementing_traits2() {
             self.level += 1;
             self.level
         }
+
+        fn print_level(&self) {
+            println!("Character `{}` has level = {}", self.name, self.level);
+        }
     }
 
     let mut durz = Character {
@@ -77,6 +81,16 @@ fn creating_traits() {
     let num_one: u16 = 3;
     let num_two: u16 = 4;
 
+    trait IsEvenOrOdd {
+        fn is_even(&self) -> bool;
+    }
+
+    impl IsEvenOrOdd for u16 {
+        fn is_even(&self) -> bool {
+            self % 2 == 0
+        }
+    }
+
     fn asserts<T: IsEvenOrOdd>(x: T, y: T) {
         assert!(!x.is_even());
         assert!(y.is_even());
@@ -94,7 +108,7 @@ fn trait_constraints_on_structs() {
         latest_version: T,
     }
 
-    impl<__> Language<T> {
+    impl<T: std::cmp::PartialOrd> Language<T> {
         fn is_stable(&self) -> bool {
             self.latest_version >= self.stable_version
         }
@@ -125,7 +139,10 @@ fn where_clause() {
         }
     }
 
-    fn asserts<T>(x: T, y: T) {
+    fn asserts<T>(x: T, y: T)
+    where
+        T: IsEvenOrOdd,
+    {
         assert!(!x.is_even());
         assert!(y.is_even());
     }
@@ -143,7 +160,7 @@ fn default_functions() {
     trait IsEvenOrOdd {
         fn is_even(&self) -> bool;
         fn is_odd(&self) -> bool {
-            __
+            true
         }
     }
 
@@ -171,12 +188,18 @@ fn inheritance() {
 
     #[derive(PartialEq)]
     struct Bawks<T> {
-        thingy: T
+        thingy: T,
     }
 
     impl<T: PartialOrd> PartialOrd for Bawks<T> {
         fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-            __
+            if self.thingy == other.thingy {
+                return Some(Ordering::Equal);
+            } else if self.thingy < other.thingy {
+                return Some(Ordering::Less);
+            } else {
+                return Some(Ordering::Greater);
+            }
         }
     }
 
