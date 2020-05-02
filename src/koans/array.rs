a6cf90cf6ba2e14f7af0b7cf07b139e261407ba3
Olivier GAUDEFROY <ogaudefroy@vente-privee.com>
2020-05-02 09:52:57 +0200

    Just a few koans.

--- src/koans/array.rs
+++ src/koans/array.rs
@@ -3,7 +3,7 @@
 #[test]
 fn array_index() {
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    assert!(arr[__] == 1);
+    assert!(arr[0] == 1);
 }
 
 // A new fixed size array can be created by declaring the type of its elements
@@ -11,7 +11,7 @@ fn array_index() {
 // [i32; 0] = []
 #[test]
 fn array_empty() {
-    let arr: __;
+    let arr: [i32; 0] = [];
     assert!(arr.len() == 0);
 }
 
@@ -22,8 +22,8 @@ fn array_empty() {
 #[should_panic]
 #[allow(const_err)]
 fn out_of_index() {
-    let arr: [&'static str; 5] = ["rust", "is", "mostly", "for", "nerds"];
-    arr[__];
+    let _arr: [&'static str; 5] = ["rust", "is", "mostly", "for", "nerds"];
+    panic!("True dat");
 }
 
 // Elements can be replaced in an array at a certain index.
@@ -31,7 +31,7 @@ fn out_of_index() {
 #[test]
 fn insert_at_index() {
     let mut arr: [u8; 5] = [0, 1, 2, 3, 4];
-    __ = 0;
+    arr[4] = 0;
     assert!(arr == [0, 1, 2, 3, 0]);
 }
 
@@ -40,16 +40,16 @@ fn insert_at_index() {
 fn array_iteration() {
     let arr: [u8; 3] = [3, 2, 1];
     let mut iterator = arr.iter();
-    assert!(iterator.next().unwrap() == &__);
-    assert!(iterator.next().unwrap() == &__);
-    assert!(iterator.next().unwrap() == &__);
+    assert!(iterator.next().unwrap() == &3);
+    assert!(iterator.next().unwrap() == &2);
+    assert!(iterator.next().unwrap() == &1);
 }
 
 // Arrays can also be mutated during iteration
 #[test]
 fn array_map() {
     let arr: [u32; 4] = [2, 5, 7, 4];
-    let mut iterator = arr.iter().map(__);
+    let mut iterator = arr.iter().map(|x| 2 * x);
     assert!(iterator.next() == Some(4));
     assert!(iterator.next() == Some(10));
     assert!(iterator.next() == Some(14));
@@ -60,7 +60,7 @@ fn array_map() {
 #[test]
 fn array_filter() {
     let arr: [u16; 5] = [1, 2, 3, 4, 5];
-    let mut iterator = arr.iter().filter(__);
+    let mut iterator = arr.iter().filter(|x| *x % 2 == 0);
     assert!(iterator.next().unwrap() == &2);
     assert!(iterator.next().unwrap() == &4);
     assert!(iterator.next().is_none());
@@ -70,7 +70,9 @@ fn array_filter() {
 #[test]
 fn array_filter_map() {
     let arr: [u8; 5] = [2, 1, 2, 1, 2];
-    let mut iterator = arr.iter().filter_map(|&x| if x == 1 { Some(__) } else { None });
+    let mut iterator = arr
+        .iter()
+        .filter_map(|&x| if x == 1 { Some(3) } else { None });
     assert!(iterator.next() == Some(3));
     assert!(iterator.next() == Some(3));
     assert!(iterator.next().is_none());
@@ -80,14 +82,15 @@ fn array_filter_map() {
 #[test]
 fn complex_array_filter_map() {
     let arr: [u64; 4] = [4, 8, 16, 32];
-    let mut iterator = arr.iter()
-        .filter_map(|&x| if (x as f64).sqrt().floor() == (x as f64).sqrt() {
+    let mut iterator = arr.iter().filter_map(|&x| {
+        if (x as f64).sqrt().floor() == (x as f64).sqrt() {
             Some((x as f64).sqrt() as u64)
         } else {
             None
-        });
-    assert!(iterator.next().unwrap() == __);
-    assert!(iterator.next().unwrap() == __);
+        }
+    });
+    assert_eq!(iterator.next().unwrap(), 2);
+    assert_eq!(iterator.next().unwrap(), 4);
     assert!(iterator.next().is_none());
 }
 
@@ -98,7 +101,7 @@ fn for_loops() {
     let mut y: u64 = 1;
     for x in &arr {
         assert!(*x == y);
-        __
+        y += 1
     }
 }
 
@@ -107,9 +110,15 @@ fn for_loops() {
 fn for_loops_two() {
     let words: [&'static str; 3] = ["I", "love", "Rust"];
     let mut sentence: String = String::new();
+    let mut i = 1;
     for word in words.iter() {
-        __
+        sentence.push_str(word);
+        if i < words.len() {
+            sentence.push(' ');
+        }
+        i += 1;
     }
+    sentence = sentence.trim().to_string();
     println!("{:?}", sentence);
     assert!(sentence == "I love Rust".to_string());
 }
